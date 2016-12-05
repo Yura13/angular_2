8a0d2f9075e39fbe3b7f7c1b973a02fc7784991e
Yura <yura61388@gmail.com>
2016-12-05 10:38:31 +0300

    unit 39

--- 01-first-app/src/app/databinding/databinding.component.ts
+++ 01-first-app/src/app/databinding/databinding.component.ts
@@ -2,8 +2,8 @@ import { Component } from '@angular/core';
 
 @Component({
   selector: 'fa-databinding',
-  templateUrl: './databinding.component.html',
-  styleUrls: ['./databinding.component.css']
+  templateUrl: 'databinding.component.html',
+  styleUrls: ['databinding.component.css']
 })
 export class DatabindingComponent {
   stringInterpolation = 'This is string interpolation!';
