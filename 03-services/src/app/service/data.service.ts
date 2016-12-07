9e5633e0e50dd0d4347ee098e2566c41765df4cb
Yurii Dutchak <yura61388@gmail.com>
2016-12-07 17:36:48 +0200

    services units 54-58

---
+++ 03-services/src/app/service/data.service.ts
@@ -0,0 +1,17 @@
+// import { Injectable } from '@angular/core';
+//
+// @Injectable()
+export class DataService {
+  private data: string[] = [];
+
+  addData(input: string) {
+    this.data.push(input);
+  }
+
+  getData() {
+    return this.data;
+  }
+
+  // constructor() { }
+
+}
